c8f3fd36131e146a64a50fc37bcc3b3f71a28461
enrique215walter-max <enrique215walter@gmail.com>
2026-02-02 21:20:38 -0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React, { useState, effect } from 'react';
+import axios from 'axios';
+import { CloudRain, Sun, Menu, Search, Heart, MapPin, Loader2 } from 'lucide-react';
+import Virgen3D from './Virgen3D';
+import Background from './Background';
+import clsx from 'clsx';
+
+export default function App() {
+  return <div>App</div>;
+}
